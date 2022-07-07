500dd6a1d67ef28c8db8e7b0339cf48f013040f4
Muniraju <muniraju.a@lge.com>
2022-07-07 16:07:12 +0530

    Migrate RP apps to use React18
    :Release Notes:
    Migrate RP apps to use React18
    
    :Detailed Notes:
    Migrate RP apps to use React18
    
    :Testing Performed:
    Verification pending
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [WRO-7546] Migrate RP apps to use React18
    
    Change-Id: If7065055947b1e5e418a973e9a8227468770def2
    Reviewed-on: http://gpro.lge.com/c/webosose/com.webos.app.videoplayer/+/332768
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Muniraju A <muniraju.a@lge.com>
    Tested-by: Muniraju A <muniraju.a@lge.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import {render} from 'react-dom';
+/* global ENACT_PACK_ISOMORPHIC */
+import {createRoot, hydrateRoot} from 'react-dom/client';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
 import App from './App';
@@ -11,9 +12,12 @@ const appElement = (
 	</Provider>
 );
 
-// In a browser environment, render instead of exporting
+// In a browser environment, render the app to the document.
 if (typeof window !== 'undefined') {
-	render(appElement, document.getElementById('root'));
+    if (ENACT_PACK_ISOMORPHIC) {
+        hydrateRoot(document.getElementById('root'), appElement);
+    } else {
+        createRoot(document.getElementById('root')).render(appElement);
+    }
 }
-
 export default appElement;
